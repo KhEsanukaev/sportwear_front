b1871a910f5faff850c9dab2205a725500346038
KhEsanukaev <halid.esanukaev.86@mail.ru>
2025-02-21 17:34:21 +0300

    Обновил компонент Header, добавил новые иконки и стили

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,16 +1,27 @@
-// src/components/Header/index.tsx
-import React from 'react';
-import { Container } from 'react-bootstrap';
-import styles from './Header.module.scss';
+
+import React from "react";
+import { Container } from "react-bootstrap";
+import styles from "./Header.module.scss";
 
 const Header: React.FC = () => {
-    return (
-        <header className={styles.header}>
-            <Container>
-                <h3>Магазин спортивной одежды</h3>
-            </Container>
-        </header>
-    );
+  return (
+    <header className={styles.header}>
+      <Container className={styles.container}>
+        <div className={styles.logo}>
+          <img src="/logo-icon.svg" alt="Иконка логотипа" />
+        </div>
+        <h3>Магазин спортивной одежды</h3>
+        <div className={styles.navbarButtons}>
+          <div className={styles.buttonIcon}>
+            <img src="/curt-icon.svg" alt="Иконка корзины" />
+          </div>
+          <div className={styles.buttonIcon}>
+            <img src="/button-icon.svg" alt="Иконка входа" />
+          </div>
+        </div>
+      </Container>
+    </header>
+  );
 };
 
 export default Header;
