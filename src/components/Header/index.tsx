3d53fa307ecf9a3557960350a08184bf38491eb5
KhEsanukaev <halid.esanukaev.86@mail.ru>
2025-02-23 19:26:50 +0300

    Обновил шапку и пакеты

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,23 +1,39 @@
+import React, {useState} from 'react';
+import { Container, Button } from 'react-bootstrap';
+import { CiShoppingCart } from 'react-icons/ci';
+import styles from '../Header/Header.module.scss'; // если у тебя есть стили
 
-import React from "react";
-import { Container } from "react-bootstrap";
-import styles from "./Header.module.scss";
+const Header = () => {
 
-const Header: React.FC = () => {
+  const [isAdmin, setIsAdmin] = useState(false);
+
+  const handleLogin = () => {
+
+    setIsAdmin(true);
+  }
+
   return (
     <header className={styles.header}>
       <Container className={styles.container}>
         <div className={styles.logo}>
-          <img src="/logo-icon.svg" alt="Иконка логотипа" />
+          <img src="/logo-icon.svg" alt="Логотип компании" />
         </div>
+
         <h3>Магазин спортивной одежды</h3>
-        <div className={styles.navbarButtons}>
-          <div className={styles.buttonIcon}>
-            <img src="/curt-icon.svg" alt="Иконка корзины" />
+
+        <div className={styles.navbar}>
+          <div className={styles.cartIcon}>
+            <CiShoppingCart size={35} />
           </div>
-          <div className={styles.buttonIcon}>
-            <img src="/button-icon.svg" alt="Иконка входа" />
-          </div>
+          {isAdmin ? (
+            <Button variant="primary" className={styles.loginButton}>
+              Личный кабинет
+            </Button>
+          ) : (
+            <Button variant="primary" className={styles.loginButton} onClick={handleLogin}>
+              Войти
+            </Button>
+          )}
         </div>
       </Container>
     </header>
